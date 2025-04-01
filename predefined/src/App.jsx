61ca5be98055179cb43ddc576669797b0453362c
Cameron Burchnall <cameron.burchnall@ntlworld.com>
2025-04-02 00:22:07 +0100

    Init commit Created array Displayed array Created function to give class to mapped array components based on value Created function to increase array item value on click

---
+++ predefined/src/App.jsx
@@ -0,0 +1,49 @@
+import "./App.css";
+import { useState } from "react";
+
+export default function App() {
+  const initialGrid = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 1, 2, 1, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+  ];
+
+  const [grid, setGrid] = useState(initialGrid);
+
+  function gridColour(Grid) {
+    if (Grid === 0) {
+      return "white-cell";
+    } else if (Grid === 1) {
+      return "grey-cell";
+    } else if (Grid === 2) {
+      return "green-cell";
+    }
+  }
+
+  const handleClick = (rowIdx, colIdx) => {
+    const newGrid = grid.map((row, r) =>
+      row.map((cell, c) => (r === rowIdx && c === colIdx ? cell + 1 : cell))
+    );
+    setGrid(newGrid);
+  };
+
+  return (
+    <div id="gridStyle">
+      {grid.map((row, rowIndex) => (
+        <div key={rowIndex} className="grid-row">
+          {row.map((value, colIndex) => (
+            <div
+              key={colIndex}
+              className={gridColour(value)}
+              onClick={() => handleClick(rowIndex, colIndex)}
+            ></div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+}
